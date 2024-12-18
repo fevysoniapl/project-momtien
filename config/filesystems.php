4fb4f4636abe52c7fba45617003520fb53681d37
unknown <fevylestari67@gmail.com>
2024-12-18 22:10:07 +0700

    db backup schedule

--- config/filesystems.php
+++ config/filesystems.php
@@ -35,6 +35,12 @@ return [
             'root' => storage_path('app'),
             'throw' => false,
         ],
+        
+        'backup' => [
+            'driver' => 'local',
+            'root' => storage_path('app/backups'),
+            'throw' => false,
+        ],
 
         'public' => [
             'driver' => 'local',
